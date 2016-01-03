f34408f74b41741996201b5c9a43ea90d1a96ebb
onebone <jyc00410@gmail.com>
2016-01-03 21:05:37 +0900

    Oops, wrong permission

--- src/main/java/me/onebone/economyapi/command/MyMoneyCommand.java
+++ src/main/java/me/onebone/economyapi/command/MyMoneyCommand.java
@@ -37,7 +37,7 @@ public class MyMoneyCommand extends Command{
 	@Override
 	public boolean execute(CommandSender sender, String label, String[] args) {
 		if(!this.plugin.isEnabled()) return false;
-		if(!sender.hasPermission("economyapi.command.topmoney")) return false;
+		if(!sender.hasPermission("economyapi.command.mymoney")) return false;
 		
 		if(!(sender instanceof Player)){
 			sender.sendMessage(TextFormat.RED+"Please use this command in-game.");
